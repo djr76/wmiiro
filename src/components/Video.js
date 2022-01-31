17587868ace10fa98a834e2cc5945a4a469c6770
djr76 <53257673+djr76@users.noreply.github.com>
2022-01-31 12:25:05 -0500

    3

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,22 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import PropTypes from "prop-types";
 
-const Video = ({ embedId }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
-);
+// const Video = ({ embedId }) => (
+//   <div className="video-responsive">
+//     <iframe
+//       width="853"
+//       height="480"
+//       src={`https://www.youtube.com/embed/${embedId}`}
+//       frameBorder="0"
+//       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//       allowFullScreen
+//       title="Embedded youtube"
+//     />
+//   </div>
+// );
 
-Video.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
+// Video.propTypes = {
+//   embedId: PropTypes.string.isRequired
+// };
 
-export default Video;
+// export default Video;
